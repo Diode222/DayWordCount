79f7f26708876dc73b873bf239a2ff2292d7203f
diode <544481521@qq.com>
2019-10-04 00:34:27 +0800

    record info

--- app/src/main/java/com/erjiguan/daywordcount/service/WechatListener.java
+++ app/src/main/java/com/erjiguan/daywordcount/service/WechatListener.java
@@ -14,15 +14,29 @@ import jackmego.com.jieba_android.RequestCallback;
 
 public class WechatListener extends AccessibilityService {
     private String inputString = "";
+    private String chatPerson = "Unknown";
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         int eventType = event.getEventType();
         CharSequence packageName = event.getPackageName();
-        if (packageName.equals("com.tencent.mm") && (eventType == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED || eventType == AccessibilityEvent.TYPE_VIEW_CLICKED) && event.getContentDescription() == null) {
-            if (eventType == AccessibilityEvent.TYPE_VIEW_CLICKED && event.getText().toString().equals("[发送]")) {
+        if (packageName.equals("com.tencent.mm") && (eventType == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED || eventType == AccessibilityEvent.TYPE_VIEW_CLICKED || eventType == AccessibilityEvent.TYPE_VIEW_FOCUSED) && event.getContentDescription() == null) {
+            if (eventType == AccessibilityEvent.TYPE_VIEW_FOCUSED && event.getText().toString().length() > 0) {
+                String chatPageItemsString = event.getText().toString();
+                String insideChatPageItemsString = chatPageItemsString.substring(1, chatPageItemsString.length() - 1).trim();
+                String[] chatPageItems = insideChatPageItemsString.split(", ");
+                if (chatPageItems.length <= 1) {
+                    return;
+                }
+                chatPerson = chatPageItems[1];
+            } else if (eventType == AccessibilityEvent.TYPE_VIEW_CLICKED && event.getText().toString().equals("[发送]")) {
                 String insideInputString = inputString.substring(1, inputString.length() - 1).trim();
                 if (insideInputString.length() > 0) {
+                    long sendTime = System.currentTimeMillis();
+                    // TODO 将数据存储逻辑写到这里，删掉下面的本地分词逻辑。这里只存微信消息、聊天对象和时间
+
+
+
                     RequestCallback<ArrayList<String> > callback = new RequestCallback<ArrayList<String>>() {
                         @Override
                         public void onSuccess(ArrayList<String> result) {
